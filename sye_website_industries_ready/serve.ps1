20daa6794c62d7b2a13c1bb346aad2ec0236f7f6
yheu16-bit <yheu16@gmail.com>
2026-08-30 15:39:21 +0300

    Add files via upload

---
+++ sye_website_industries_ready/serve.ps1
@@ -0,0 +1,48 @@
+$root = "C:\Users\USER\Downloads\sye_website_industries_ready"
+$port = 8532
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add("http://localhost:$port/")
+$listener.Start()
+Write-Host "Serving $root on http://localhost:$port/"
+
+$mime = @{
+  ".html" = "text/html; charset=utf-8"
+  ".css"  = "text/css"
+  ".js"   = "application/javascript"
+  ".png"  = "image/png"
+  ".jpg"  = "image/jpeg"
+  ".jpeg" = "image/jpeg"
+  ".svg"  = "image/svg+xml"
+  ".gif"  = "image/gif"
+  ".ico"  = "image/x-icon"
+  ".woff" = "font/woff"
+  ".woff2"= "font/woff2"
+}
+
+while ($listener.IsListening) {
+  $context = $listener.GetContext()
+  $req = $context.Request
+  $res = $context.Response
+  try {
+    $path = $req.Url.LocalPath
+    if ($path -eq "/") { $path = "/index.html" }
+    $filePath = Join-Path $root ($path.TrimStart("/"))
+    if (Test-Path $filePath -PathType Leaf) {
+      $ext = [System.IO.Path]::GetExtension($filePath)
+      $contentType = $mime[$ext]
+      if (-not $contentType) { $contentType = "application/octet-stream" }
+      $bytes = [System.IO.File]::ReadAllBytes($filePath)
+      $res.ContentType = $contentType
+      $res.ContentLength64 = $bytes.Length
+      $res.OutputStream.Write($bytes, 0, $bytes.Length)
+    } else {
+      $res.StatusCode = 404
+      $msg = [System.Text.Encoding]::UTF8.GetBytes("404 Not Found")
+      $res.OutputStream.Write($msg, 0, $msg.Length)
+    }
+  } catch {
+    $res.StatusCode = 500
+  } finally {
+    $res.OutputStream.Close()
+  }
+}
